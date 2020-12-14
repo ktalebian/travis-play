c4dbfa9291a8433331d8793938e72e54afa760e0
Kousha Talebian <kousha.talebian@gmail.com>
2020-12-13 17:06:52 -0800

    convert to lerna

---
+++ packages/travis-play-package-one/src/index.ts
@@ -0,0 +1,6 @@
+export default function() {
+    return {
+        name: 'one',
+        env: process.env,
+    }
+}
---
+++ packages/travis-play-package-two/src/index.ts
@@ -0,0 +1,6 @@
+export default function() {
+    return {
+        name: 'two',
+        env: process.env,
+    }
+}
